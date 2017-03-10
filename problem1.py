2fbad738051598f46c70c75d6f967d0d0eace6ea
Andrew Lehmann <andrewlehmann@users.noreply.github.com>
2017-03-09 23:50:29 -0600

    slight reformat

--- problem1.py
+++ problem1.py
@@ -18,15 +18,12 @@ def Majority(A):
 	if len(A) == 1:
 		print(A[0])
 		return [A[0]]
-
 	elif len(A) == 2:
 		if A[0] == A[1]:
 			print(A[0])
 			return [A[0]]
-
 		else:
 			return A
-
 	else:
 		A1 = []
 		A2 = []
@@ -34,39 +31,31 @@ def Majority(A):
 
 		for i in range(0, mid):
 			A1.append(A[i])
-
 		for j in range(mid, len(A)):
 			A2.append(A[j])
 
 		print("A1, A2: ", A1, A2)
-
 		R1 = Majority(A1)
 		R2 = Majority(A2)
-
 		print("R1, R2: ", R1, R2)
+
 		if len(A1) == 1:
 			if len(A2) == 1:
 				return Majority(merge(R1, R2))
-
 			else:
 				if R1[0] in R2:
 					return [R1[0]]
-
 				else:
 					return Majority(R2)
-
 		else:
 			if len(A2) == 1:
 				if R2[0] in R1:
 					print("R2: ", R2[0])
 					return [R2[0]]
-
 				else:
 					return Majority(R1)
-
 			else:
 				return Majority(merge(R1, R2))
-
 
 def main():
 	A = [5, 6, 3, 5, 5, 5, 1, 5, 5, 6, 8, 5]
